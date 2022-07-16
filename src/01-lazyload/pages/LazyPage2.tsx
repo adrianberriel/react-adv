a8e55399590ecb10625c5261c51a1fd2c70b0b28
Adrian Berriel <berriel@gmail.com>
2022-07-16 12:40:49 -0300

    Componentes Lazy agregados

---
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -0,0 +1,5 @@
+export const LazyPage2 = () => {
+    return (
+        <h1>LazyPage 2</h1>
+    )
+}
---
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -0,0 +1,5 @@
+export const LazyPage3 = () => {
+    return (
+        <h1>LazyPage 3</h1>
+    )
+}
